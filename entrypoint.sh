0e0b533b89c89586c6ecff78e70de497a65d3d7f
Thomas Liske <thomas@fiasko-nw.net>
2018-01-03 23:02:59 +0100

    [docker] Create runtime directorys.

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,7 @@
 groupadd -g "${NBGID}" nbgroup
 useradd -u "${NBUID}" -g "${NBGID}" nbuser
 
+mkdir -p /var/run/najabo /var/spool/najabo
+chown "${NBUID}:${NBGID}" /var/run/najabo /var/spool/najabo
+
 exec su -p -s /usr/bin/perl nbuser -- $@
