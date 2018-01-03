81fc946934481e331b1310d26ef6e6969f654afd
Thomas Liske <thomas@fiasko-nw.net>
2018-01-03 23:45:04 +0100

    Release version 0.6.4.

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ useradd -u "${NBUID}" -g "${NBGID}" nbuser
 
 # prepare ownership
 chown "${NBUID}:${NBGID}" /var/run/najabo /var/spool/najabo
-for fn in najabo.conf najabo.vcard
+for fn in najabo.conf najabo.vcard; do
     test -e "/etc/najabo/$fn" || cp -vp "/etc/najabo.dist/$fn" "/etc/najabo/$fn"
     chown "${NBUID}:${NBGID}" "/etc/najabo/$fn"
 done
